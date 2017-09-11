85ded3361252dfa0d543a1caeeeaa9513b3e1f5e
trietdang <trietdang@cca.edu>
2017-09-11 13:38:44 -0700

    hello
    sacxbhjsv

---
+++ 50-buttons/assets/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+// $('#button-23').click(function(){
+// 	$(this).fadeOut();
+// });
+
+// $('#button-24').click(function(){
+// 	$('.item').fadeIn();
+// });
+
+?(this).
+
+});
