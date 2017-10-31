a9570258a07f65bdd0a43132b95b14ea4b18dbe6
trietdang <trietdang@cca.edu>
2017-10-31 15:55:35 -0700

    final

---
+++ prooject-2/js/script.js
@@ -0,0 +1,234 @@
+$(document).ready(function(){
+
+
+$( ".acontainer" ).hover(
+  function() {
+  $(".acircle1").addClass('canimation'), 
+  $(".acircle2").addClass('canimation'), 
+  $(".astroke1").addClass('acurve1'), 
+  $(".astroke2").addClass('acurve2') },
+  function(){ }
+)
+
+
+$( ".bcontainer" ).hover(
+  function() {
+    $(".bcircle1").addClass('canimation'), 
+    $(".bcircle2").addClass('canimation'), 
+    $(".bstroke2").addClass('bcurve2'), 
+    $(".bstroke3").addClass('bcurve3') },
+  function(){ }
+)
+
+$( ".ccontainer" ).hover(
+  function() {
+    $(".ccircle1").addClass('canimation'), 
+    $(".cstroke1").addClass('ccurve1') },
+  function(){ }
+)
+
+$( ".dcontainer" ).hover(
+  function() {
+    $(".dcircle1").addClass('canimation'), 
+    $(".dstroke2").addClass('dcurve1') },
+  function(){ }
+)
+
+$( ".econtainer" ).hover(
+  function() {
+    $(".ecircle1").addClass('canimation'), 
+    $(".estroke1").addClass('ecurve1') },
+  function(){ }
+)
+
+$( ".fcontainer" ).hover(
+  function() {
+    $(".fcircle1").addClass('canimation'),
+    $(".fcircle2").addClass('canimation'), 
+    $(".fstroke1").addClass('fcurve1') },
+  function(){ }
+)
+
+$( ".gcontainer" ).hover(
+  function() {
+    $(".gcircle1").addClass('canimation'),
+    $(".gcircle2").addClass('canimation'), 
+    $(".gstroke1").addClass('gcurve1'),
+    $(".gstroke2").addClass('gcurve2') },
+  function(){ }
+)
+
+$( ".hcontainer" ).hover(
+  function() {
+    $(".hcircle1").addClass('canimation'),
+    $(".hcircle2").addClass('canimation'), 
+    $(".hstroke1").addClass('hcurve1'),
+    $(".hstroke2").addClass('hcurve2') },
+  function(){ }
+)
+
+$( ".icontainer" ).hover(
+  function() {
+    $(".icircle1").addClass('canimation'),
+    $(".icircle2").addClass('canimation'), 
+    $(".istroke1").addClass('icurve1') },
+  function(){ }
+)
+
+$( ".jcontainer" ).hover(
+  function() {
+    $(".jcircle1").addClass('canimation'), 
+    $(".jstroke2").addClass('jcurve2') },
+  function(){ }
+)
+
+$( ".kcontainer" ).hover(
+  function() {
+    $(".kcircle1").addClass('canimation'), 
+    $(".kcircle2").addClass('canimation'),
+    $(".kstroke1").addClass('kcurve1'),
+    $(".kstroke3").addClass('kcurve2') },
+  function(){ }
+)
+
+$( ".lcontainer" ).hover(
+  function() {
+    $(".lcircle1").addClass('canimation'), 
+    $(".lstroke1").addClass('lcurve1') },
+  function(){ }
+)
+
+$( ".mcontainer" ).hover(
+  function() {
+    $(".mcircle1").addClass('canimation'), 
+    $(".mcircle2").addClass('canimation'),
+    $(".mstroke1").addClass('mcurve1'),
+    $(".mstroke3").addClass('mcurve3') },
+  function(){ }
+)
+
+$( ".ncontainer" ).hover(
+  function() {
+    $(".ncircle1").addClass('canimation'), 
+    $(".ncircle2").addClass('canimation'),
+    $(".nstroke1").addClass('ncurve1'),
+    $(".nstroke2").addClass('ncurve2') },
+  function(){ }
+)
+
+$( ".ocontainer" ).hover(
+  function() {
+    $(".ocircle1").addClass('canimation'),
+    $(".ocircle2").addClass('canimation'),
+    $(".ostroke1").addClass('ocurve1'),
+    $(".ostroke2").addClass('ocurve2') },
+  function(){ }
+)
+
+$( ".pcontainer" ).hover(
+  function() {
+    $(".pcircle1").addClass('canimation'),
+    $(".pcircle2").addClass('canimation'),
+    $(".pstroke1").addClass('pcurve1'),
+    $(".pstroke2").addClass('pcurve2') },
+  function(){ }
+)
+
+$( ".qcontainer" ).hover(
+  function() {
+    $(".qcircle1").addClass('canimation'),
+    $(".qcircle2").addClass('canimation'),
+    $(".qstroke1").addClass('qcurve1'),
+    $(".qhoz2").addClass('qcurve2') },
+  function(){ }
+)
+
+$( ".rcontainer" ).hover(
+  function() {
+    $(".rcircle1").addClass('canimation'),
+    $(".rcircle2").addClass('canimation'),
+    $(".rstroke1").addClass('rcurve1'),
+    $(".rstroke3").addClass('rcurve2') },
+  function(){ }
+)
+
+$( ".scontainer" ).hover(
+  function() {
+    $(".scircle1").addClass('canimation'),
+    $(".scircle2").addClass('canimation'),
+    $(".scircle3").addClass('canimation'),
+    $(".sstroke1").addClass('scurve1'),
+    $(".sstroke4").addClass('scurve2') },
+  function(){ }
+)
+
+$( ".tcontainer" ).hover(
+  function() {
+    $(".tcircle1").addClass('canimation'),
+    $(".tstroke1").addClass('tcurve1') },
+  function(){ }
+)
+
+$( ".ucontainer" ).hover(
+  function() {
+    $(".ucircle1").addClass('canimation'),
+    $(".ustroke2").addClass('ucurve1') },
+  function(){ }
+)
+
+$( ".vcontainer" ).hover(
+  function() {
+    $(".vcircle1").addClass('canimation'),
+    $(".vhoz1").addClass('vcurve1') },
+  function(){ }
+)
+
+$( ".wcontainer" ).hover(
+  function() {
+    $(".wcircle1").addClass('canimation'),
+    $(".wcircle2").addClass('canimation'),
+    $(".wcircle3").addClass('canimation'),
+    $(".wstroke1").addClass('wcurve1'),
+    $(".wstroke2").addClass('wcurve2'),
+    $(".wstroke3").addClass('wcurve3') },
+  function(){ }
+)
+
+$( ".xcontainer" ).hover(
+  function() {
+    $(".xcircle1").addClass('canimation'),
+    $(".xcircle2").addClass('canimation'),
+    $(".xcircle3").addClass('canimation'),
+    $(".xcircle4").addClass('canimation'),
+    $(".xstroke1").addClass('xcurve1'),
+    $(".xstroke2").addClass('xcurve2'),
+    $(".xstroke3").addClass('xcurve3'),
+    $(".xstroke4").addClass('xcurve4') },
+  function(){ }
+)
+
+$( ".ycontainer" ).hover(
+  function() {
+    $(".ycircle1").addClass('canimation'),
+    $(".ycircle2").addClass('canimation'),
+    $(".ystroke1").addClass('ycurve1'),
+    $(".ystroke3").addClass('ycurve2') },
+  function(){ }
+)
+
+$( ".zcontainer" ).hover(
+  function() {
+    $(".zcircle1").addClass('canimation'),
+    $(".zcircle2").addClass('canimation'),
+    $(".zhoz1").addClass('zcurve1'),
+    $(".zhoz2").addClass('zcurve2') },
+  function(){ }
+)
+
+$( ".fullcontainer" ).hover(
+  function() {
+    $(".fulle").addClass('fullshow')},
+  function(){ }
+)
+
+});
